f079ab3df1e125dedf15f386229f37a54d495d48
Alexander Schmidt <alexander.schmidt@schmidt-buch-verlag.de>
2015-11-28 13:31:27 +0100

    Put the logger into a separate module

--- index.js
+++ index.js
@@ -2,23 +2,7 @@
 
 // import libraries
 var http = require('http');
-var EventEmitter = require('events');
-
-// colours constansts
-const COLOR_RED = '\x1b[31;1m';
-const COLOR_BLUE = '\x1b[36;1m';
-const COLOR_RESET = '\x1b[0m';
-
-
-// create custom logger
-var logger = new EventEmitter();
-logger.on('info', function (message) {
-    console.log(COLOR_BLUE, message, COLOR_RESET);
-});
-logger.on('error', function (message) {
-    console.log(COLOR_RED, message, COLOR_RESET);
-});
-
+var logger = require('./logger.js');
 
 // create simple webserver
 var server = http.createServer(function (request, response) {
