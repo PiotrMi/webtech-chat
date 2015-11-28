7fb4fb0453f12de2d25e6ced5e22f4217cbbef2b
Davide Pedranz <davide.pedranz@gmail.com>
2015-11-28 13:58:03 +0100

    Serve index.html

--- index.js
+++ index.js
@@ -2,18 +2,34 @@
 
 // import libraries
 var http = require('http');
+var fs = require('fs');
 var logger = require('./logger.js');
 
-// create simple webserver
+// create simple webserver: now serve the index.html
 var server = http.createServer(function (request, response) {
 
     // log request
     logger.emit('info', '>> new request: ' + request.url);
 
-    // send respose
-    response.writeHead(418);
-    response.write('I\'m a teapot\n');
-    response.end();
+    // read the file
+    fs.readFile(__dirname + '/public/index.html', function (err, data) {
+
+        // log the possible error
+        if (err) {
+            logger.emit('error', 'error reading index.html -> ' + err);
+            response.writeHead(404);
+        }
+
+        // return the file content
+        else {
+            response.writeHead(200);
+            response.write(data);
+        }
+
+        // end the response
+        response.end();
+    });
+
 });
 
 // run webserver
